a809b267705190b7b63cc99224cd0775831df01f
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2020-08-16 11:21:55 +0700

    change file name

--- xampp.sh
+++ xampp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/7.4.8/xampp-linux-x64-7.4.8-0-installer.run/download
+sudo wget -O xampp-linux-x64-7.4.8-0-installer.run https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/7.4.8/xampp-linux-x64-7.4.8-0-installer.run/download
 chmod 755 xampp-linux-x64-7.4.8-0-installer.run
 sudo ./xampp-linux-x64-7.4.8-0-installer.run
