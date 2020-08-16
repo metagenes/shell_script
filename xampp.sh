ae048e39223db55835f8d35e91a155bb6f224553
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2020-08-16 11:14:43 +0700

    add xampp

---
+++ xampp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo wget https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/7.4.8/xampp-linux-x64-7.4.8-0-installer.run/download
+chmod 755 xampp-linux-x64-7.4.8-0-installer.run
+sudo ./xampp-linux-x64-7.4.8-0-installer.run
